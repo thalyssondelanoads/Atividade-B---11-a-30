0bbc536e1f432b948ee8f7a69906b7d6ed6b17f9
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-14 01:40:51 -0300

    Questão 25

---
+++ questao25.py
@@ -0,0 +1,7 @@
+print('Transformar M para Km|M')
+print('------')
+m = float(input('Qual o valor em M? : '))
+km = int(m // 1000)
+m2 = int(m % 1000)
+print('-------')
+print(f' O resultado é {km} Quilômetros e {m2} Metros ! ')
