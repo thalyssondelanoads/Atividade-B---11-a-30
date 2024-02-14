86561bf9c09a47369be6049ef59186ad838e85ba
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-14 01:01:05 -0300

    Questao 18

---
+++ questao18.py
@@ -0,0 +1,7 @@
+print('Calcular comprimento da circunferência ')
+print('------')
+raio = float(input('Qual o valor do raio? : '))
+π = 3
+comprimento = π * raio * 2 
+print('------')
+print(f'O comprimento dessa circunferência é {comprimento} ! ')
