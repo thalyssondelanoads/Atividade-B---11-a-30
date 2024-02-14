580dc7c4dd8f2dd45d05bb35d09fd91aecdafc71
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-14 00:24:35 -0300

    Atualizar o questao13.py

--- questao13.py
+++ questao13.py
@@ -2,4 +2,5 @@ print('Calcular 70% de um valor em Real (R$)')
 print('------')
 real = float(input('Digite o valor: '))
 result = real * 7/10
-print(f' 70% de {real} é igual a {result} Reais ')
+print('------')
+print(f'70% de {real} é igual a {result} Reais ')
