91af7868062373edbc5142f041bc43ec136e2b04
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-14 19:07:00 -0300

    Atualizar o teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,19 @@
-print('Calcular distância entre 2 pontos no plano ') 
+print('Calcular distância entre 2 pontos no plano !')
 print('-----')
-print('Infome as coordenadas de x1 e y1')
+
+print('Informe as coordenadas de x1 e y1')
 x1 = float(input('x1: '))
 y1 = float(input('y1: '))
 print('-----')
-print('Informe as coordenadas de x2 e y2 ')
+
+print('Informe as coordenadas de x2 e y2')
 x2 = float(input('x2: '))
 y2 = float(input('y2: '))
-print('----')
-dist = int((( x2 - x1 ) ** 2 ) + ((y2 - y1 ) ** 2))
+print('-----')
+
+dist = int(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
 print(f'A distância entre os 2 pontos é √{dist} !')
+aprox = round(float(dist ** 0.5), 2)
+print('-----')
+
+print(f'Dessa forma, o valor exato ou aproximado da distância é {aprox} !')
