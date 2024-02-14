10c79baa6f039496778d58ae8da9743fe262bd4e
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-14 01:46:23 -0300

    Questão 26

---
+++ questao26.py
@@ -0,0 +1,7 @@
+print('Transformar Dias em Semanas|Dias')
+print('------')
+dia = int(input('Quantos Dias? : '))
+sem = dia // 7
+dia_rest = dia % 7
+print('------')
+print(f'O resultado é {sem} Semanas e {dia_rest} Dias ! ')
