ea95a4edc75186d054f4179dfdad52d570ac3593
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:26:38 -0300

    Atualizar o questao19.py

--- questao19.py
+++ questao19.py
@@ -1,8 +1,11 @@
 print('Calcular volume de uma esfera')
 print('------')
+
 raio = float(input('Qual valor do raio? : '))
+
 π = 3.14
 v1 = 4 * π * (raio**3)
 vfinal = int(v1 / 3)
+
 print('-----')
 print(f'O volume dessa esfera é aproximadamente {vfinal}! ')
