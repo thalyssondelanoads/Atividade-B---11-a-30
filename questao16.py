45267f33bc26700cc7e470d7cda522a573e9ac96
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:25:05 -0300

    Atualizar o questao16.py

--- questao16.py
+++ questao16.py
@@ -1,6 +1,9 @@
 print('Calcular a Área de um Quadrado ')
 print('------')
+
 lado = float(input('Qual valor do lado do quadrado? : '))
+
 area = lado ** 2
+
 print('------')
 print(f'A Área desse Quadrado é {area} ! ')
