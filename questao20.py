e6c22626834f61a8c1126464340bba6bd8e26a3c
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:27:54 -0300

    Atualizar o questao20.py

--- questao20.py
+++ questao20.py
@@ -1,6 +1,8 @@
 print('Transformar °C em °F')
 print('------')
+
 c = float(input('Qual a temperatura em °C? : '))
 f = int((9 * c + 160)/5)
+
 print('-----')
 print(f'{c} °C são aproximadamente {f} °F ! ')
