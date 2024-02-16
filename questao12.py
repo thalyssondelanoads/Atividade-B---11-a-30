229cc9952cc92ec027d49dd4786146cc0b6cd977
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:22:08 -0300

    Atualizar o questao12.py

--- questao12.py
+++ questao12.py
@@ -1,7 +1,10 @@
 print('Aumento de Salário ')
 print('---------')
+
 salario_atual = float(input('Digite o seu salário atual: '))
 print('----------')
+
 aumento = salario_atual * 1/4
 salario_novo = int(salario_atual + aumento)
+
 print(f'Seu novo salário é {salario_novo} Reais, aproveite !')
