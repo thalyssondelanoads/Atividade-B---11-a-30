c6695a79c25faf5c8bf48b0e14ae53584489026c
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:25:37 -0300

    Atualizar o questao17.py

--- questao17.py
+++ questao17.py
@@ -1,7 +1,10 @@
 print('Calcular área do retângulo ')
 print('-----')
+
 base = float(input('Qual valor da base? : '))
 altura = float(input('Qual valor da altura? : '))
+
 area = base * altura
+
 print('------')
 print(f'A Área desse Retângulo é {area} ! ')
