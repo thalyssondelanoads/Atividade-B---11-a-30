11de2f1c0a3dbb417c940da7e313cac11188f7a7
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:18:09 -0300

    Atualizar o questao11.py

--- questao11.py
+++ questao11.py
@@ -1,10 +1,11 @@
 print('Inventer um número inteiro de 3 dígitos')
 print('---------')
-print('O número precisa ter 3 dígitos')
-print('---------')
+
 num = int(input('Digite o Número: '))
 print('---------')
+
 alg1 = num // 100
 alg2 = num % 100 // 10
 alg3 = num % 10
+
 print(f'A inversão do seu número é {alg3}{alg2}{alg1} !')
