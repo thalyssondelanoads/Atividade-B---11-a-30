c2b3ec58c5fbb9893c84769597b32b3d821bb5a4
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:23:54 -0300

    Atualizar o questao14.py

--- questao14.py
+++ questao14.py
@@ -1,5 +1,6 @@
 print('Calculadora de Média Ponderada ')
 print('-------')
+
 nt1 = float(input('Qual sua nota 1? : '))
 p1 = float(input('Qual o peso da nota 1? : '))
 print('-------')
@@ -8,8 +9,10 @@ p2 = float(input('Qual o peso da nota 2? : '))
 print('-------')
 nt3 = float(input('Qual sua nota 3? : '))
 p3 = float(input('Qual o peso da nota 3? : '))
+
 soma_notas = ( nt1 * p1 ) + ( nt2 * p2 ) + ( nt3 * p3 )
 soma_pesos = (p1 + p2 + p3 )
 media_pond = soma_notas / soma_pesos
+
 print('------')
 print(f'Sua média é {media_pond} ! ')
