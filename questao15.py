64fe6cb2b82b69211fe03bbec4bc7dccb6347240
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:24:12 -0300

    Atualizar o questao15.py

--- questao15.py
+++ questao15.py
@@ -1,7 +1,9 @@
 print('Calcular a Área de um Triângulo ')
 print('-------')
+
 base = float(input('Qual o valor da base? : '))
 altura = float(input('Qual o valor da altura? : '))
 area = ( base * altura / 2 )
+
 print('-----')
 print(f'A área desse triângulo é {area} !')
