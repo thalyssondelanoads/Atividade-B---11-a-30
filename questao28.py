a81889946371489aee2e27d0c50bafc1bbb52ca8
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-19 21:52:31 -0300

    Update questao28.py

--- questao28.py
+++ questao28.py
@@ -1,10 +1,10 @@
 print('Transformar Horas em Semanas,Dias e Horas')
 print('------------')
-total_horas = int(input("Digite o número de horas: "))
+total_horas = int(input('Digite o número de horas: '))
 
 semanas = total_horas // 168  # 1 semana = 7 dias * 24 horas
 dias = (total_horas % 168) // 24
 horas = total_horas % 24
 
 print('-----------')
-print(f"{total_horas} horas correspondem a {semanas} semanas, {dias} dias e {horas} horas!")
+print(f'{total_horas} horas correspondem a {semanas} semanas, {dias} dias e {horas} horas! ')
