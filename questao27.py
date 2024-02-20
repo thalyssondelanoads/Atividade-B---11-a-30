19285f64b3ad37f4ed82f4da80964f5413aba7de
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-19 21:20:33 -0300

    Create questao27.py

---
+++ questao27.py
@@ -0,0 +1,11 @@
+print('Transformar segundos em Horas,Minutos e Segundos')
+print('----------')
+
+total_segundos = int(input("Digite o número de segundos: "))
+
+horas = total_segundos // 3600
+minutos = (total_segundos % 3600) // 60
+segundos = total_segundos % 60
+
+print('-------------')
+print(f"{total_segundos} segundos correspondem a {horas} horas, {minutos} minutos e {segundos} segundos!")
